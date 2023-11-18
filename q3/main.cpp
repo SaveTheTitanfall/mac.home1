3ab52e3e279d9b30a546488749464ee13f651806
SaveTheTitanfall <prokopenkoi574@gmail.com>
2023-11-18 22:46:54 +0200

    Domashka_1

---
+++ q3/main.cpp
@@ -0,0 +1,49 @@
+//Вивести на консоль текст “I love C++!”;
+
+#include <iostream>
+
+#include <vector>
+
+#include <fstream>
+
+
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+cout << "I love С++!";
+
+//}
+
+//2.Намалювати у консолі символом “*” прямокутний трикутник;
+
+#include <iostream>
+
+#include <vector>
+
+#include <fstream>
+
+
+
+using namespace std;
+
+
+
+//int main()
+
+//{
+
+cout << " *" << endl;
+
+cout << " **" << endl;
+
+cout << " ***" << endl;
+
+cout << " ****" << endl;
+
+}
